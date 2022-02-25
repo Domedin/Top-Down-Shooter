cebf02eec84ded4e067d13955260cbca77507af7
Domedin <jackanderson080307@gmail.com>
2022-02-25 08:33:37 -0500

    adding dashing
    I finally got dashing working after three hours.

--- Escape the forest/Assets/Scripts/PlayerMovement.cs
+++ Escape the forest/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,14 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     Vector2 movement;
 
+    //dash variables
+    private float activeMoveSpeed;
+    private float dashSpeed = 25f;
+    private float dashLength = 0.2f;
+    private float dashCoolDown = 3f;
+    private float dashCounter;
+    private float dashCoolCounter;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -19,12 +27,41 @@ public class PlayerMovement : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
         movement = Vector2.ClampMagnitude(movement, 1);
     }
+    private void dash()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1) && dashCoolCounter <= 0 && dashCounter <= 0)
+        {
+            
+            
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+                Debug.Log("Detecting space");
+            }
+        }
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+            if (dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashCoolCounter = dashCoolDown;
+            }
+        }
+
+        if (dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
+    }
     void Update()
     {
         Move();
+        dash();
     }
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.velocity = movement * activeMoveSpeed;
     }
 }
